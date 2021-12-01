e92c263bc89375f8f4a4022aefe46eaa72a55d2c
thanguiaido <95372910+thanguiaido@users.noreply.github.com>
2021-12-02 00:16:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "THÂN GỬI HÒA",
+    introTitle: 'HELLO HÒA',
+    introDesc: `Sau tất cả mọi chuyện 
+Điều mà tôi muốn nói với em rằng 
+Mong cho sau này chúng ta sẽ luôn ở bên nhau 
+Tôi Yêu Em Bằng tất cả tấm lòng ❤️`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: ' ĐÃ CHỜ RẤT LÂU RỒI ',
+    desc: ' NẾU CÓ CƠ HỘI BẠN CÓ THỂ MỞ LÒNG ĐÓN NHẬN TÔI ĐƯỢC KHÔNG ? ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'MÌNH CẢM ƠNNNNN',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/LMH09.08.2003' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
